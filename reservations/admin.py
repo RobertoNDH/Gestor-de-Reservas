477d4d7e905a2fade04b5e18fd6793238500f327
RobertoNDH <robertonauzet@gmail.com>
2026-04-20 23:17:57 +0100

    feat: enhance django admin with filters and search

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from .models import Resource, Availability, Booking
 
-admin.site.register(Resource)
-admin.site.register(Availability)
-admin.site.register(Booking)
+@admin.register(Resource)
+class ResourceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'resource_type', 'capacity')
+    list_filter = ('resource_type',)
+    search_fields = ('name',)
+
+@admin.register(Availability)
+class AvailabilityAdmin(admin.ModelAdmin):
+    list_display = ('resource', 'get_day_of_week_display', 'start_time', 'end_time')
+    list_filter = ('resource', 'day_of_week')
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('resource', 'user', 'start_time', 'end_time', 'is_cancelled')
+    list_filter = ('resource', 'is_cancelled', 'start_time')
+    search_fields = ('user__username', 'resource__name')
