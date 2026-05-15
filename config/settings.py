addf1b7625baab70370842873a4309960ada054c
RobertoNDH <robertonauzet@gmail.com>
2026-05-15 10:35:46 +0100

    fix: downgrade whitenoise storage to bypass missing jazzmin sourcemaps

--- config/settings.py
+++ config/settings.py
@@ -88,7 +88,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
